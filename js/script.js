7b958e4a3dce066049cdfe17d21ed63df953fe7e
Drew <drew@finstrom.us>
2015-06-01 16:05:51 -0700

    Add add task functionality.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-
+var tasks = [];
 
 //jQuery
+
+
 $( document ).ready(function() {
+
+  $("form#addTask").submit(function(event) {
+    event.preventDefault();
+
+    var name = $("input#name").val();
+    var newTask = {name: name, isComplete: false}
+    tasks.push(newTask);
+    displayTasks();
+  });
+
+
+  var displayTasks = function() {
+    $("ul#complete").empty();
+    $("ul#toDo").empty();
+    tasks.forEach(function(task) {
+
+      var li = "<li>" + task.name + "</li>"
+      if (task.isComplete) {
+        $("ul#complete").append(li);
+      } else {
+        $("ul#toDo").append(li);
+      }
+    });
+  };
+
+
 
 });
