54bd84a47d2e86775362a7d6237ad71b0f8dcf80
Drew <drew@finstrom.us>
2015-06-01 15:32:22 -0700

    set up initial styling and containers

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
-//raw js
-var foo = function(bar) {
-  "use strict";
-  return false;
-};
 
 
 //jQuery
 $( document ).ready(function() {
-  "use strict";
-    console.log( "jQuery Ready" );
 
-    $("#jqtest").text("jQuery Ready");
 });
